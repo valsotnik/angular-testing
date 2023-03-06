33f89276e23d1114f48b1b5b09032edb34365805
cm_val <cmvaltime@gmail.com>
2023-03-06 11:30:25 +0100

    refactor: add spy for logger service

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,11 +2,13 @@ import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 describe("CalculatorService", () => {
   it("should add 2 numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
+    const calculator = new CalculatorService(logger);
 
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract 2 numbers", () => {
