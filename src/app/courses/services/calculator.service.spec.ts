da577bedb804252a3ebf16f89e5eb90c7c6fb13a
cm_val <cmvaltime@gmail.com>
2023-03-06 11:29:02 +0100

    tests: add simple tests for calculator service

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,19 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+describe("CalculatorService", () => {
+  it("should add 2 numbers", () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4);
+  });
+
+  it("should subtract 2 numbers", () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(2, 2);
+
+    expect(result).toBe(0, "Unexpected subtraction result");
+  });
+});
