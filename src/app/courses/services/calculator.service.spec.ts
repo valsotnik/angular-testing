1022f129fe2346b2b4c63e7af478e6fe97db5bb2
cm_val <cmvaltime@gmail.com>
2023-03-06 11:38:37 +0100

    refactor: use beforeEach for service instances

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,21 +1,24 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 describe("CalculatorService", () => {
-  it("should add 2 numbers", () => {
-    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
-    const calculator = new CalculatorService(logger);
+  let calculator: CalculatorService, loggerSpy: any;
 
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+    calculator = new CalculatorService(loggerSpy);
+  });
+
+  it("should add 2 numbers", () => {
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract 2 numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
-
     const result = calculator.subtract(2, 2);
 
     expect(result).toBe(0, "Unexpected subtraction result");
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
