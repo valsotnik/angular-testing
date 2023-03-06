05028044bfdb5fde3e1dc1fe798f74c57294c0e6
cm_val <cmvaltime@gmail.com>
2023-03-06 15:24:01 +0100

    update: add beforeEach with waitForAsync construction for component instantiation

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,9 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  waitForAsync,
+} from "@angular/core/testing";
 import { CoursesCardListComponent } from "./courses-card-list.component";
 import { CoursesModule } from "../courses.module";
 import { COURSES } from "../../../../server/db-data";
@@ -9,8 +14,22 @@ import { Course } from "../model/course";
 import { setupCourses } from "../common/setup-test-data";
 
 describe("CoursesCardListComponent", () => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [CoursesModule],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+      });
+  }));
+
   it("should create the component", () => {
-    pending();
+    expect(component).toBeTruthy();
   });
 
   it("should display the course list", () => {
